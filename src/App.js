820fae369a0d874e0a407bb311aa74012e1a3a25
Khai Phan Van <phan_van_khai123@yahoo.com>
2021-03-18 18:35:01 +0700

    Reading data from Firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Cart from './components/Cart';
 import Navbar from './components/Navbar';
+import firebase from 'firebase';
 
 class App extends Component {
     constructor() {
@@ -8,29 +9,56 @@ class App extends Component {
         console.log("This is constructor");
         this.state = {
             products: [
-                {
-                    id: 1,
-                    price: 1000,
-                    title: 'Oppo Phone',
-                    qty: 1,
-                    img: 'https://www.flaticon.com/svg/vstatic/svg/1721/1721963.svg?token=exp=1616028004~hmac=8400b04785757b2b75bc58749b259d29'
-                },
-                {
-                    id: 2,
-                    price: 2000,
-                    title: 'Vinfast Phone',
-                    qty: 2,
-                    img: 'https://www.flaticon.com/svg/vstatic/svg/1721/1721912.svg?token=exp=1616028005~hmac=dc3acbb39cd8550cf4af87b4dc0fc9fc'
-                },
-                {
-                    id: 3,
-                    price: 3000,
-                    title: 'Bi Phone',
-                    qty: 3,
-                    img: 'https://www.flaticon.com/svg/vstatic/svg/1721/1721908.svg?token=exp=1616028009~hmac=e0825712e14e0e956810cedf2fae71cf'
-                }
-            ]
+                // {
+                //     id: 1,
+                //     price: 1000,
+                //     title: 'Oppo Phone',
+                //     qty: 1,
+                //     img: 'https://www.flaticon.com/svg/vstatic/svg/1721/1721963.svg?token=exp=1616028004~hmac=8400b04785757b2b75bc58749b259d29'
+                // },
+                // {
+                //     id: 2,
+                //     price: 2000,
+                //     title: 'Vinfast Phone',
+                //     qty: 2,
+                //     img: 'https://www.flaticon.com/svg/vstatic/svg/1721/1721912.svg?token=exp=1616028005~hmac=dc3acbb39cd8550cf4af87b4dc0fc9fc'
+                // },
+                // {
+                //     id: 3,
+                //     price: 3000,
+                //     title: 'Bi Phone',
+                //     qty: 3,
+                //     img: 'https://www.flaticon.com/svg/vstatic/svg/1721/1721908.svg?token=exp=1616028009~hmac=e0825712e14e0e956810cedf2fae71cf'
+                // }
+            ],
+            loading: true
         }
+    }
+
+    componentDidMount() {
+        firebase
+            .firestore()
+            .collection('products')
+            .get()
+            .then((snapshot) => {
+                console.log(snapshot);
+
+                snapshot.docs.map((doc) => {
+                    console.log(doc.data());
+                });
+
+                const products = snapshot.docs.map((doc) => {
+                    const data = doc.data();
+
+                    data['id'] = doc.id
+                    return data;
+                });
+
+                this.setState({
+                    products,
+                    loading: false
+                });
+            })
     }
 
     handleInCreaseQuantity = (product) => {
@@ -55,7 +83,6 @@ class App extends Component {
         products[index].qty -= 1;
         this.setState({
             products
-
         });
     }
 
@@ -93,32 +120,23 @@ class App extends Component {
         return cartTotal;
     }
 
-    componentDidMount() {
-        console.log("Component Did Mount");
-        const { products } = this.state;
-        this.setState({
-            products
-        });
-    }
 
     render() {
-
         console.log("Rendering...")
         console.log("This is state:", this.state);
         console.log("This is props:", this.props);
-        const { products } = this.state;
+        const { products, loading } = this.state;
         return (
             <div className="App" >
                 <h1>Cart</h1>
-                <Navbar
-                    count={this.getCartCount()}
-                />
+                <Navbar count={this.getCartCount()} />
                 <Cart
                     products={products}
                     onIncreasQuantity={this.handleInCreaseQuantity}
                     onDecreasQuantity={this.handleDecreasQuantity}
                     onDeleteQuantity={this.handleDeleteProduct}
                 />
+                {loading && <h1>Loading Products...</h1>}
                 <div style={{ padding: 10, fontSize: 20 }}>Total:{this.getCartTotal()}</div>
             </div>
         );
